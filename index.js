872ebf608a19d7a81f7cc8aec11c264e906020f3
github username <github email address>
2021-04-05 12:35:45 -0400

    commit tree 02a7396cc7b5649f59a6ae02fa442ec2b77cf184

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+if (value <= 400) {
+  return 'This one is on me!';
+} else if (value > 2500) {
+  return 'No can do.';
+} else if (value > 2000) {
+  return 'I will gladly take your thirty bucks.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const response = city === 'NYC' ? 'Ok, sounds good.':'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+let response;
+  switch (tip) {
+  case 'generous':
+    response = "Thank you so much.";
+    break;
+  case 'not as generous':
+    response = "Thank you."
+    break;
+  default:
+    response = "Bye."
+}
+return response;
 }
